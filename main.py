8f89a66b6cc0fc7237464ea04cfc77e134921993
norouzy <mohamadnewday@gmail.com>
2021-12-29 20:43:56 +0330

    set error

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 import hashlib,json,requests,re
 from requests.structures import CaseInsensitiveDict
+import PySimpleGUI as sg
+import os
+import threading
+
+
+sg.theme("DarkAmber")
+arr = ['','','','']
+layout = [
+		[sg.T("")],
+		[sg.Text("Game link    : ",text_color='white',key="-link-"), sg.InputText(key="-IN1-" ,change_submits=True,size=(50,50))],
+		[sg.Text("Score :          ",text_color='white',key="-score-"), sg.InputText(key="-IN2-" ,change_submits=True,size=(10,10))],
+		[sg.Text("Time  :          ",text_color='white',key="-time-"), sg.InputText(key="-IN3-" ,change_submits=True,size=(10,10))],
+		[sg.Text("Results :                   ",text_color='white',key="-result-",visible=False),sg.Multiline(" ",key="-run-",visible=False,auto_size_text=True,text_color='white',size=(50,20))],
+		[sg.T("")],[sg.T("")],
+		[sg.T(""),sg.T(""),sg.T(""),sg.T(""),sg.T(""),sg.T(""),sg.T(""),sg.T(""),sg.T(""),sg.T(""),sg.T(""),sg.Button("Submit", button_color=('white', 'green'),border_width=5) , sg.Button("Cancel",button_color=('white', 'red'),border_width=5)],
+		[sg.T("")],
+		[sg.T("")],
+		[sg.Text("produce by norouzy    29/12/2021",text_color='white', font='Courier 8')]
+	]
+window = sg.Window('Gamee cheat', layout,icon='backup_icon-icons.com_72047.ico', resizable=True, finalize=True)
+
 
 
 def get_checksum(score,playTime,url):
@@ -69,6 +90,7 @@ def game_id(game_url):
 	result_data = resp.json()
 	return result_data['result']['game']['id']
 
+
 def send_score(score,timePlay,checksum,token,game_url,game_id):
 	url = "http://api.service.gameeapp.com"
 
@@ -88,14 +110,28 @@ def send_score(score,timePlay,checksum,token,game_url,game_id):
 	headers["Sec-Fetch-Site"] = "cross-site"
 	headers["Te"] = "trailers"
 	headers["Authorization"] = "Bearer {my_token}".format(my_token=token)
-	#  eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOiIxNjQwNjUyNTE4IiwidXNlcklkIjoxNTQzMDIzOSwiaW5zdGFsbFV1aWQiOiJlNjgyZjhkYS02ZjY3LTRjODQtYTI5My1mMDk0YjBkYzc0ZGYiLCJ0eXBlIjoiYXV0aGVudGljYXRpb25Ub2tlbiIsImF1dGhvcml6YXRpb25MZXZlbCI6ImJvdCIsInBsYXRmb3JtIjoiYm90LXRlbGVncmFtIn0.VkWEgipqHWjpT01tebhn-oOj0sPD8c_W7UBnE9KTgfo
 	data = '{"jsonrpc":"2.0","id":"game.saveWebGameplay","method":"game.saveWebGameplay","params":{"gameplayData":{"gameId":'+str(game_id)+',"score":'+str(score)+',"playTime":'+str(timePlay)+',"gameUrl":"'+game_url+'","metadata":{"gameplayId":30},"releaseNumber":8,"gameStateData":null,"createdTime":"2021-12-28T03:20:24+03:30","checksum":"'+checksum+'","replayVariant":null,"replayData":null,"replayDataChecksum":null,"isSaveState":false,"gameplayOrigin":"game"}}}'
 
 
 	resp = requests.post(url, headers=headers, data=data)
 
 	print(resp.status_code)
-	print(resp.content)
+	result_text = ""
+	status = 0
+	my_json = resp.json()
+	keys_list = list(my_json)
+	for i in keys_list:
+		if i == "error":
+			result_text = my_json['error']['message']+"\n"+my_json['error']['data']['reason']+"\n"+"try after "+my_json['user']['cheater']['banStatus']
+			status = 1
+			break
+
+	if status == 0:
+		user_posin_rank = my_json['result']['surroundingRankings'][0]['ranking']
+		for user in user_posin_rank:
+			result_text = str(user['rank'])+" - "+ user['user']['firstname']+" "+user['user']['lastname']+" score : "+str(user['score'])+"\n"+result_text
+	return result_text
+
 
 def game_link(url):
 	pattern = r"https:\/\/prizes\.gamee\.com(\/game-bot\/.*)#tg"
@@ -103,13 +139,23 @@ def game_link(url):
 	link = result.groups(0)[0]
 	return link
 
-url = "https://prizes.gamee.com/game-bot/rollerdisco-34106e5c0f111a182e5afe0ddede5c39f2be4fa3#tgShareScoreUrl=tgb%3A%2F%2Fshare_game_score%3Fhash%3DhHJUbMEoNfxVRmVmJorD"
-game_url = game_link(url)
-print(game_url)
-exit()
-score = 12104
-time = 1309
-token = get_token(game_url)
-checksum = get_checksum(score, time, game_url)
-game_id = game_id(game_url)
-send_score(score, time, checksum, token, game_url, game_id)
+
+while True:
+	event, values = window.read()
+
+	if event == sg.WIN_CLOSED or event=="Exit" or event =="Cancel":                                                                                        
+		window.close()
+		break
+	elif event == "Submit":
+		window['-run-'].update(visible =True)
+		game_url = game_link(values['-IN1-'])
+		score = values['-IN2-']
+		time = values['-IN3-']
+		token = get_token(game_url)
+		checksum = get_checksum(score, time, game_url)
+		game_id = game_id(game_url)
+		result = send_score(score, time, checksum, token, game_url, game_id)
+		window['-result-'].update(visible =True)
+		window['-run-'].update(result)
+
+
